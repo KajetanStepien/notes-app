627e76554b7808b6aa10eb8552a3db35cbecf18b
Kajetan Stępień <kajetanstepien@MacBook-Air-Kajetan.local>
2025-03-06 12:43:19 +0100

    fixed archives, each user now has unique archive

--- back-end/server.js
+++ back-end/server.js
@@ -28,6 +28,92 @@ server.post("/login", (req, res) => {
   }
 });
 
+server.post("/folders", (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const userId = decoded.userId;
+
+    const { name } = req.body;
+
+    const newFolder = {
+      id: Date.now(),
+      name,
+      userId,
+    };
+
+    router.db.get("folders").push(newFolder).write();
+    res.json(newFolder);
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+});
+
+server.post("/archive", (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const userId = decoded.userId;
+
+    const { title, body, folderId } = req.body;
+
+    const newNote = {
+      id: Date.now(),
+      title,
+      body,
+      folderId: Number(folderId),
+      userId,
+    };
+
+    router.db.get("archive").push(newNote).write();
+    res.json(newNote);
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+});
+
+server.get("/archive", (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const userId = decoded.userId;
+
+    const archive = router.db.get("archive").filter({ userId }).value();
+    res.json(archive);
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+});
+
+server.get("/folders", (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const userId = decoded.userId;
+
+    const folders = router.db.get("folders").filter({ userId }).value();
+    res.json(folders);
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+});
+
 server.use((req, res, next) => {
   if (req.headers.authorization) {
     const token = req.headers.authorization.split(" ")[1];
